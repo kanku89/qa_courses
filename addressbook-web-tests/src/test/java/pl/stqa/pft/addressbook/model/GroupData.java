1ec16f47b40b9f445ae76f734af92c66924d1476
Konrad <fijalkowski.konrad@gmail.com>
2019-10-30 21:28:11 +0100

    Added list assertion - comparison.

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -1,5 +1,7 @@
 package pl.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String name;
   private final String header;
@@ -15,11 +17,33 @@ public class GroupData {
     return name;
   }
 
+
+
   public String getHeader() {
     return header;
   }
 
   public String getFooter() {
     return footer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+        "name='" + name + '\'' +
+        '}';
   }
 }
