41c44093b54934e7cc8e1f7c1595c5b0ae2d13b2
Konrad <fijalkowski.konrad@gmail.com>
2019-11-24 14:17:18 +0100

    Data generator + data provider for groups

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -9,23 +9,25 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class ContactCreationTest extends TestBase {
 
+
+
   @Test
   public void testNewContact() {
     app.getNavigationHelper().goHome();
     Contacts before = app.getContactHelper().all();
     app.getNavigationHelper().addNewContact();
+//    File photo = new File();
     ContactData contact = new ContactData()
         .withFirstName("First Name")
         .withLastName("Last Name")
         .withAddress("Address")
         .withMobile("123456789")
         .withMail("mail@mail.com");
+//        .withPhoto(photo);
     app.getContactHelper().createNewContact(contact);
     assertThat(app.getContactHelper().contactsCount(), equalTo(before.size() + 1));
     Contacts after = app.getContactHelper().all();
     assertThat(after, equalTo(before.withAdded(contact)));
-
-
   }
 
 }
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,8 +1,20 @@
 package pl.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 import pl.stqa.pft.addressbook.model.Groups;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,11 +22,38 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+    List<Object[]> list = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+    }.getType());
+    return groups.stream().map(g -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromJson")
+  public void testGroupCreation(GroupData group) {
     app.getNavigationHelper().goToGroups();
     Groups before = app.getGroupHelper().all();
-    GroupData group = new GroupData().withName("Test2");
     app.getGroupHelper().createGroup(group);
     assertThat(app.getGroupHelper().groupsCount(), equalTo(before.size() + 1));
     Groups after = app.getGroupHelper().all();
@@ -25,3 +64,4 @@ public class GroupCreationTests extends TestBase {
   }
 
 }
+
