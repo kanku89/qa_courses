6031c58889e8be5a55e8006bee4e549437e90187
Konrad <fijalkowski.konrad@gmail.com>
2019-12-08 19:27:32 +0100

    Added new module for Mantis tests.

---
+++ mantis-tests/src/test/java/pl/stqa/pft/manits/appmanager/ApplicationManager.java
@@ -0,0 +1,79 @@
+package pl.stqa.pft.manits.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+
+public class ApplicationManager {
+  private final Properties properties;
+  WebDriver driver;
+
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.FIREFOX)) {
+      driver = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      driver = new ChromeDriver();
+    } else {
+      driver = new InternetExplorerDriver();
+    }
+
+
+    driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+    driver.get(properties.getProperty("web.baseUrl"));
+
+
+  }
+
+  public void stop() {
+    driver.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  public void acceptAlert() {
+    try {
+      driver.switchTo().alert().accept();
+    } catch (NoAlertPresentException e) {
+      System.out.println("No alert is present, error: " + e);
+    }
+  }
+
+}
